1f3dbbdc57763a19d99a732608e3a1475229ea24
Yoshihiro Yamazaki <yoya@awm.jp>
2012-10-26 16:58:18 +0900

    - Shape => mStruct.Shape - new Shape 1st argument(withStyle) true to false.

--- src/structs/Font.js
+++ src/structs/Font.js
@@ -1,14 +1,14 @@
 /**
  * @author Yoshihiro Yamazaki
  *
- * Copyright (C) 2012 Yoshihiro Yamazaki
+ * Copyright (C) 2012 QuickSWF Project
  * This code is licensed under the zlib license. See LICENSE for details.
  */
 (function(global) {
 
   global.quickswf.structs.Font = Font;
 
-  var Shape = global.quickswf.structs.Shape;
+  var mStruct = global.quickswf.structs;
 
   /**
    * @constructor
@@ -25,12 +25,13 @@
    * @return {quickswf.structs.Font} The loaded Font.
    */
   Font.load = function(pReader, pOffsetOfOffsetTable, pOffsetTable) {
+    var Shape = mStruct.Shape;
     var tFont = new Font();
     var tNumGlyphs = pOffsetTable.length;
     var tGlyphShapeTable = new Array(tNumGlyphs);
     for (var i = 0 ; i < tNumGlyphs ; i++) {
         pReader.seekTo(pOffsetOfOffsetTable + pOffsetTable[i]);
-        var tShape = Shape.load(pReader, true, false, false);
+        var tShape = Shape.load(pReader, false, false, false);
         tGlyphShapeTable[i] = tShape;
     }
     tFont.shapes = tGlyphShapeTable;
