985c24d6122b98bb5e4b1cd04a5c935d4583d9c8
Kuu MIYAZAKI <kuu.miyazaki@gree.net>
2012-11-20 17:19:07 +0900

    Changes for Font and Text integration.

--- src/displaylist-structs/Shape.js
+++ src/displaylist-structs/Shape.js
@@ -39,6 +39,9 @@
     if (pWithStyles) {
       tShape.fillStyles = mStructs.FillStyle.loadMultiple(pReader, pWithAlpha, pHasLargeFillCount, false);
       tShape.lineStyles = mStructs.LineStyle.loadMultiple(pReader, pWithAlpha, pHasLargeFillCount, false);
+    } else {
+      tShape.fillStyles = [new mStructs.FillStyle(false)];
+      tShape.lineStyles = [new mStructs.FillStyle(false)];
     }
     tShape.numberOfFillBits = pReader.bp(4);
     tShape.numberOfLineBits = pReader.bp(4);
