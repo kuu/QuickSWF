381cfc9bbc06391158618cd464687b4dacfae101
Kuu MIYAZAKI <kuu.miyazaki@gree.net>
2013-03-14 18:21:05 +0900

    Reworked based on the feedback. (https://github.com/Moncader/QuickSWF/pull/44).

--- src/actions/DoAction.js
+++ src/actions/DoAction.js
@@ -35,7 +35,7 @@
     var tReader = new global.Breader(pBuffer),
         tActionCode, tLength, tSWF = pParser.swf,
         tLiteralTypeOffset, tUint8Array, tBase64String,
-        tString, tStringLength, tReadLength;
+        tStringLength, tStartIndex, tType;
 
     while ((tActionCode = tReader.B()) !== 0) {
       // We are only interested in ActionPush (type=0, string literal)
@@ -44,39 +44,64 @@
         continue;
       }
       tLength = tReader.I16();
+
+      // Skip the actions other than Push.
       if (tActionCode !== 0x96) {
         tReader.seek(tLength);
         continue;
       }
-      tLength--; // Minus 1 byte for the action code.
-      if (tReader.B() !== 0) { // Type (0 : String)
-        tReader.seek(tLength);
-        continue;
-      }
 
-      // Looping here because the string can be null-terminated in the middle.
-      tReadLength = 0;
-      while (tReadLength < tLength) {
-        // Skip the string literals other than Shift-JIS.
-        //if (tReader.s(true, pParser.nonUtf8CharDetected) !== null) {
-        tStringLength = tReader.sl();
-        tString = tReader.s(true, pParser.nonUtf8CharDetected);
-        if (tString !== null) {
-          // Utf8 char:
-          // The string is not used and later the same string is going to be parsed again.
-          // This sounds inefficient, but we want to avoid storing and searching a lot of strings.
-        } else {
-          // Non-Utf8 char:
-          // Make a request to convert Shift-JIS to UCS.
-          pParser.nonUtf8CharDetected = true;
-          tLiteralTypeOffset = tReader.tell() - 1;
-          tUint8Array = tReader.sub(tReader.tell(), tStringLength);
-          tBase64String = global.btoa(global.String.fromCharCode.apply(null, tUint8Array));
-          tReader.seek(tStringLength + 1);
-          tSWF.mediaLoader.load(tBase64String, tUint8Array, 'text/plain; charset=Shift_JIS');
-          pBuffer[tLiteralTypeOffset] = 255; // Overwrite the literal type.
+      // ActionPush
+
+      tStartIndex = tReader.tell();
+
+      while (tReader.tell() - tStartIndex < tLength) {
+        tType = tReader.B();
+        switch (tType) {
+        case 1: // Floating Point literal
+          tReader.F32();
+          break;
+        case 4: // Register Number
+          tReader.B();
+          break;
+        case 5: // Boolean
+          tReader.B() ? true : false;
+          break;
+        case 6: // Double
+          tReader.F64();
+          break;
+        case 7: // Integer
+          tReader.I32();
+          break;
+        case 8: // Constant8: For constant pool index < 256
+          tReader.B();
+          break;
+        case 9: // Constant16: For constant pool index >= 256
+          tReader.I16();
+          break;
+        case 0: // String literal
+          // Shift-JIS -> UCS conversion.
+          if (tReader.s(true, pParser.nonUtf8CharDetected) !== null) {
+            // Utf8 char:
+            // The string is not used and later the same string is going to be parsed again.
+            // This sounds inefficient, but we want to avoid storing and searching a lot of strings.
+          } else {
+            // Non-Utf8 char:
+            // Make a request to convert Shift-JIS to UCS.
+            pParser.nonUtf8CharDetected = true;
+            tLiteralTypeOffset = tReader.tell() - 1;
+            tStringLength = tReader.sl();
+            tUint8Array = tReader.sub(tReader.tell(), tStringLength);
+            tBase64String = global.btoa(global.String.fromCharCode.apply(null, tUint8Array));
+            tReader.seek(tStringLength + 1);
+            tSWF.mediaLoader.load(tBase64String, tUint8Array, 'text/plain; charset=Shift_JIS');
+            pBuffer[tLiteralTypeOffset] = 255; // Overwrite the literal type.
+          }
+          break;
+        default:
+          console.warn('[ActionPush] ---- Unknown data type. ----');
+          break;
         }
-        tReadLength += (tStringLength + 1);
       }
     }
   }
